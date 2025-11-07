3b5c1ea4a49c6d78c48a1ad7d68101cd674274e0
Priyanshu Rawat <priyanshu847700@gmail.com>
2025-11-07 20:13:37 +0530

    .exe files
    Signed-off-by: Priyanshu Rawat <priyanshu847700@gmail.com>

---
+++ 05.loops/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+//while loop!!
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i=0;
+//     cout<<"enter the val of n : ";
+//     cin>>n;
+//     while(i<=n){
+//         cout<<i<<endl;
+//         i++;
+//     }
+//     return 0;
+// }
